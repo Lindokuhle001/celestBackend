574a13aaec98fb943523d663f413b1ee74135803
lindokuhle shabalala <mlixshaba@gmail.com>
2022-05-27 14:43:30 +0200

    finnished login endpoint

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const { DateTime } = require("luxon");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const clientId= process.env.CLIENT_ID;
-const baseURL= process.env.BASE_URL;
-const tokenEndPoint= process.env.TOKEN_END_POINT;
-const userDetailsEndPoint= process.env.USER_DETAILS_END_POINT;
+const clientId = process.env.CLIENT_ID;
+const baseURL = process.env.BASE_URL;
+const tokenEndPoint = process.env.TOKEN_END_POINT;
+const userDetailsEndPoint = process.env.USER_DETAILS_END_POINT;
+const secretToken = process.env.SECRET_TOKEN;
+const secretMerchantId =  process.env.MERCHANT_ID
 
 app.use(express.json());
+// app.use(verify)
 
 /* menu, user and price apis */
 // const maxTables = 10
@@ -103,56 +107,97 @@ app.use(express.json());
 //     res.send(user)
 // })
 
-const date = ()=> DateTime.now().toISO();
 
-const requestFunction = async(requestBody,path) =>{
-    const headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-        "client-id": clientId,
-        "request-time": date(),
-        "Signature": "algorithm=RSA256, keyVersion=1, signature=testing_signatur",
-    };
-    
-    const options = {
+const verify =(err,req, res, next) =>{
+    if(req.path === "/login"){
+        console.log("login middleware");
+         verifyMerchant(req, res, next)
+    }else{
+        console.log("test middleware");
+        verifyToken(req, res, next)
+    }
+}
+
+const verifyMerchant = (req, res, next) => {
+    // console.log(req);
+    const {merchantid } = req.headers;
+    // console.log(req.headers);
+    if (!merchantid) return res.sendStatus(401)
+    if(merchantid === secretMerchantId) {
+        next()
+    }else{
+        res.sendStatus(403)
+    }
+}
+
+const verifyToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  const token = authorization && authorization.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secretToken, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+app.use(verify)
+
+
+const date = () => DateTime.now().toISO();
+
+const requestFunction = async (requestBody, path) => {
+  const headers = {
+    "Content-Type": "application/json; charset=UTF-8",
+    "client-id": clientId,
+    "request-time": date(),
+    Signature: "algorithm=RSA256, keyVersion=1, signature=testing_signatur",
+  };
+
+  const options = {
     method: "POST",
     url: path,
     headers,
-    data:requestBody,
-    };
+    data: requestBody,
+  };
 
-    const response = await axios(options);
-    return response.data;
+  const response = await axios(options);
+  return response.data;
+};
 
-}
-
-
-
-app.post("/token", async (req, res) => {
+app.post("/login", async (req, res) => {
   const { authCode } = req.body;
-  const path =
-  `${baseURL}${tokenEndPoint}`;
+  // console.log("login path");
 
-  const body = JSON.stringify({
+  const accessTokenPath = `${baseURL}${tokenEndPoint}`;
+  const accessTokenBody = JSON.stringify({
     grantType: "AUTHORIZATION_CODE",
     authCode,
-    });
-  const token = await requestFunction(body,path)
- res.send(token)
-});
-
-
-app.post("/userDetails", async (req, res) => {
-  const { accessToken } = req.body;
-  const path =
-  `${baseURL}${userDetailsEndPoint}`;
-
-  const body = JSON.stringify({
-    accessToken
   });
-  const user = await requestFunction(body,path);
-  
- res.send(user.userInfo)
+  const accessTokenResponse = await requestFunction(
+    accessTokenBody,
+    accessTokenPath
+  );
+  const {accessToken} = accessTokenResponse;
+
+  const userDetailsPath = `${baseURL}${userDetailsEndPoint}`;
+  const userDetailsBody = JSON.stringify({
+    accessToken,
+  });
+  const userDetails = await requestFunction(userDetailsBody, userDetailsPath);
+  const userInfo = userDetails;
+
+  const jsonWebToken = jwt.sign(userInfo, secretToken);
+  res.header("authorization", `${jsonWebToken}`);
+  res.send(userInfo);
 });
+
+app.post("/test", verifyToken, (req, res) => {
+  res.send("success");
+});
+
 
 
 app.listen(port, () => {
