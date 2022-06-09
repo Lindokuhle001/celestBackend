c3420effcfc3186141cf91d992a34b46c65557df
lindokuhle shabalala <mlixshaba@gmail.com>
2022-06-09 11:01:01 +0200

    added payment and ecription

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const fs = require("fs");
 const express = require("express");
 const { verify } = require("./services/middleware");
 const crypto = require("crypto");
-const buffer = require("buffer");
-const {
-  makeVodapayRequest,
-  signToken,
-  getRequestDate,
-} = require("./services/helperFunctions");
+const { makeVodapayRequest, signToken } = require("./services/helperFunctions");
 
 const {
   PORT: port = 3000,
@@ -18,15 +13,14 @@ const {
   USER_DETAILS_END_POINT: userDetailsEndPoint,
   SECRET_TOKEN: secretToken,
   PAYMENT_END_POINT: paymentEndPoint,
-  // PRIVATE_KEY: privateKey,
   CLIENT_ID: clientId,
 } = process.env;
 
 const app = express();
-app.use(express.json());
 // app.use(verify);
+app.use(express.json());
 
-/* menu, user and price apis */
+// menu, user and price apis
 // const maxTables = 10
 // const getTable = (max) => {
 //     return Math.floor(Math.random() * max) +1
@@ -112,7 +106,7 @@ app.use(express.json());
 
 // })
 //
-/* user */
+// user
 // app.get("/user", (req, res) => {
 //     res.send(user)
 // })
@@ -120,24 +114,26 @@ app.use(express.json());
 app.post("/login", async (req, res) => {
   const { authCode } = req.body;
   console.log("login path");
-  console.log(authCode);
+  console.log(authCode, "clientId");
 
   const accessTokenPath = `${baseURL}${tokenEndPoint}`;
-  const accessTokenBody = JSON.stringify({
+  const accessTokenBody = {
     grantType: "AUTHORIZATION_CODE",
     authCode,
-  });
+  };
+
   const accessTokenResponse = await makeVodapayRequest(
     accessTokenBody,
     accessTokenPath
   );
+  console.log("accessTokenResponse");
   const { accessToken } = accessTokenResponse;
-  console.log(accessTokenResponse);
+  console.log("accessToken");
 
   const userDetailsPath = `${baseURL}${userDetailsEndPoint}`;
-  const userDetailsBody = JSON.stringify({
+  const userDetailsBody = {
     accessToken,
-  });
+  };
   const userDetails = await makeVodapayRequest(
     userDetailsBody,
     userDetailsPath
@@ -149,117 +145,12 @@ app.post("/login", async (req, res) => {
 });
 
 app.post("/pay", async (req, res) => {
-  // const { authCode } = req.body;
-  // console.log("payment path");
+  const path = `${baseURL}${paymentEndPoint}`;
+  const requestBody = req.body;
 
-  // const accessTokenPath = `${baseURL}${tokenEndPoint}`;
-  // const accessTokenBody = JSON.stringify({
-  //   grantType: "AUTHORIZATION_CODE",
-  //   authCode,
-  // });
-  // const accessTokenResponse = await makeVodapayRequest(
-  //   accessTokenBody,
-  //   accessTokenPath
-  // );
-  // const { accessToken } = accessTokenResponse;
-
-  // const userDetailsPath = `${baseURL}${userDetailsEndPoint}`;
-  // const userDetailsBody = JSON.stringify({
-  //   accessToken,
-  // });
-  // const userDetails = await makeVodapayRequest(
-  //   userDetailsBody,
-  //   userDetailsPath
-  // );
-  // const userInfo = userDetails;
-
-  // const paymentPath = `${baseURL}${paymentEndPoint}`;
-  // const paymentBody = JSON.stringify({
-  //   productCode: "CASHIER_PAYMENT",
-  //   salesCode: "51051000101000000011",
-  //   paymentRequestId: "c0a83b17161398737179310015310",
-  //   paymentNotifyUrl: "https://www.merchant.com/paymentNotifyxxx",
-  //   paymentRedirectUrl: "https://www.merchant.com/redirectxxx",
-  //   paymentExpiryTime: "2022-02-22T17:49:31+08:00",
-  //   paymentAmount: {
-  //     currency: "ZAR",
-  //     value: "6234",
-  //   },
-  //   order: {
-  //     goods: {
-  //       referenceGoodsId: "goods123",
-  //       goodsUnitAmount: {
-  //         currency: "ZAR",
-  //         value: "6234",
-  //       },
-  //       goodsName: "mobile1",
-  //     },
-  //     env: {
-  //       terminalType: "MINI_APP",
-  //     },
-  //     orderDescription: "Car",
-  //     buyer: {
-  //       referenceBuyerId: "216610000000259832353",
-  //     },
-  //   },
-  // });
-  // const requestBody = req.body;
-  // const requestBody = {
-  //   order: {
-  //     orderId: "OrderID_0101010101",
-  //     orderDescription: "sample_order",
-  //     orderAmount: {
-  //       value: "100",
-  //       currency: "JPY",
-  //     },
-  //   },
-  //   paymentAmount: {
-  //     value: "100",
-  //     currency: "JPY",
-  //   },
-  //   paymentFactor: {
-  //     isInStorePayment: "true",
-  //   },
-  // };
-  const unsignedContent = `POST ${paymentEndPoint}
-  ${clientId}.${getRequestDate()}.${requestBody}`;
-
-  fs.readFile("testCredentials/rsa_private_key.PEM", (err, key) => {
-    const privateKey = key;
-    // create key with crypto
-    // create signer with crypto RSA- SHA256
-    // write unsigned payload to the singer
-    // signature = signer sign with base 64
-
-    const data = Buffer.from(unsignedContent);
-    const sign = crypto.sign("SHA256", data, privateKey);
-    const signature = sign.toString("base64");
-    console.log(signature);
-  });
-
-  // const signature = crypto
-  //   .createHmac("sha256", privateKey)
-  //   .update(unsignedContent)
-  //   .digest("hex");
-
-  //   console.log(signature);
-
-  //   const headers = {
-  //     "Content-Type": "application/json; charset=UTF-8",
-  //     "client-id": clientId,
-  //     "request-time": getRequestDate(),
-  //   Signature: `algorithm=RSA256, keyVersion=0, signature=${signature}`,
-  // };
-
-  //   const options = {
-  //     method: "POST",
-  //     url: `${baseURL}${paymentEndPoint}`,
-  //     headers,
-  //     data: requestBody,
-  //   };
-
-  //   const response = await axios(options);
-  //   console.log(response);
+  const response = await makeVodapayRequest(requestBody, path);
+  // console.log(response.data);
+  res.send(response.data);
 });
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -294,25 +185,15 @@ app.post("/pay", async (req, res) => {
 // const signature = sign.toString("base64");
 // console.log(signature);
 
-// // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 app.post("/encription", async (req, res) => {
   const requestBody = req.body;
+  const path =
+    "https://vodapay-gateway.sandbox.vfs.africa/v2/authorizations/applyTokenSigned";
 
-  const unsignedContent = `POST ${paymentEndPoint}
-  ${clientId}.${getRequestDate()}.${requestBody}`;
-
-  fs.readFile("testCredentials/rsa_private_key.PEM", (err, data) => {
-    if (err) throw err;
-
-    const privateKey = crypto.createPrivateKey(data);
-    const sign = crypto.createSign("RSA-SHA256");
-    sign.write(unsignedContent);
-    sign.end();
-    const signature = sign.sign(privateKey, "base64");
-    console.log(signature);
-    res.send(signature);
-  });
+  const response = await makeVodapayRequest(requestBody, path);
+  res.send(response);
 });
 
 app.post("/test", (req, res) => {
--- services/helperFunctions.js
+++ services/helperFunctions.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
+const crypto = require("crypto");
+
 const { DateTime } = require("luxon");
 const { CLIENT_ID: clientId } = process.env;
-const jwt = require("jsonwebtoken"); 
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const signToken = (userInfo, secretToken) => {
   return jwt.sign(userInfo, secretToken);
@@ -9,12 +12,33 @@ const signToken = (userInfo, secretToken) => {
 
 const getRequestDate = () => DateTime.now().toISO();
 
+const getSignature = (requestBody, requestTime, endPoint) => {
+  const unsignedContent = `POST ${endPoint}\n${clientId}.${requestTime}.${JSON.stringify(
+    requestBody
+  )}`;
+
+  // console.log(unsignedContent);
+  const key = fs.readFileSync("rsa_private_key.PEM", "utf8");
+
+  const privateKey = crypto.createPrivateKey(key);
+  const sign = crypto.createSign("RSA-SHA256");
+  sign.write(unsignedContent);
+  sign.end();
+  const signature = sign.sign(privateKey, "base64");
+  // console.log(signature);
+  return signature;
+};
+
 const makeVodapayRequest = async (requestBody, path) => {
+  const requestTime = getRequestDate();
+  const uriPath = new URL(path).pathname;
+  const signature = getSignature(requestBody, requestTime, uriPath);
+
   const headers = {
     "Content-Type": "application/json; charset=UTF-8",
     "client-id": clientId,
-    "request-time": getRequestDate(),
-    Signature: "algorithm=RSA256, keyVersion=1, signature=testing_signatur",
+    "request-time": requestTime,
+    signature: `algorithm=RSA256, keyVersion=1, signature=${signature}`,
   };
 
   const options = {
@@ -24,8 +48,11 @@ const makeVodapayRequest = async (requestBody, path) => {
     data: requestBody,
   };
 
-  const response = await axios(options);
-  return response.data;
+  const response = await axios(options).catch((err) => {
+    console.log(err);
+  });
+  // console.log(response);
+  return response;
 };
 
 module.exports = { makeVodapayRequest, signToken, getRequestDate };
