61656bff3fe1527fbfe9265b650d4ecd2817815d
lindokuhle shabalala <mlixshaba@gmail.com>
2022-06-27 11:21:05 +0200

    implimented mvc

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const { authorise } = require("./services/middleware");
-const { makeVodapayRequest, signToken } = require("./services/helperFunctions");
-const { menu, price } = require("./services/database");
-const { createOrder } = require("./services/orderFunction");
+const {
+  priceController,
+  paymentController,
+  loginController,
+  menuController,
+  paymentNotificationController,
+} = require("./controller/controllers");
 
 const app = express();
 app.use(express.json());
@@ -18,57 +22,15 @@ const {
   PAYMENT_END_POINT: paymentEndPoint,
 } = process.env;
 
-app.post("/price", (req, res) => {
-  res.send(price);
-});
+app.post("/price", priceController);
 
-app.post("/menu", (req, res) => {
-  res.send(menu);
-});
+app.post("/menu", menuController);
 
-app.post("/login", async (req, res) => {
-  console.log(req.body);
-  const { authCode } = req.body;
+app.post("/login", loginController);
 
-  const accessTokenPath = `${baseURL}${tokenEndPoint}`;
-  const accessTokenBody = {
-    grantType: "AUTHORIZATION_CODE",
-    authCode,
-  };
+app.post("/payment-notification", paymentNotificationController);
 
-  const accessTokenResponse = await makeVodapayRequest(
-    accessTokenBody,
-    accessTokenPath
-  );
-  const { accessToken } = accessTokenResponse.data;
-
-  const userDetailsPath = `${baseURL}${userDetailsEndPoint}`;
-  const userDetailsBody = {
-    accessToken,
-  };
-  const userDetails = await makeVodapayRequest(
-    userDetailsBody,
-    userDetailsPath
-  );
-  const userInfo = userDetails.data;
-  const jsonWebToken = signToken(userInfo, secretToken);
-  res.send({ userInfo, jsonWebToken });
-});
-
-app.post("/payment-notification", async (req, res) => {
-  console.log(req.body.data);
-  // const notification = req.body;
-  // res.send(notification);
-});
-
-app.post("/pay", async (req, res) => {
-  const path = `${baseURL}${paymentEndPoint}`;
-  const { referenceBuyerId, orderDescription, value } = req.body;
-  const order = createOrder(referenceBuyerId, orderDescription, value);
-
-  const response = await makeVodapayRequest(order, path);
-  res.send(response.data);
-});
+app.post("/pay", paymentController);
 
 app.listen(port, () => {
   console.log(`app listening on port: ${port}`);
